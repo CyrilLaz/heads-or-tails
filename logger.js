deb31b3cecf9cf2cfa0f6966569166430ed09d30
Cyril_Lazar <klazar@mail.ru>
2024-03-04 23:42:27 +0700

    feat:добавил set для файла лога

--- logger.js
+++ logger.js
@@ -1,13 +1,17 @@
 module.exports = class {
-  constructor(path) {
+  constructor(path='') {
     this.file = path;
   }
 
   fs = require("fs");
+  file;
+
+  set file(f) {
+    this.file = f;
+  }
 
   async write(content) {
     const timeStamp = (Date.now() / 1000).toFixed();
-
     await new Promise((res, rej) =>
       this.fs.appendFile(
         this.file,
