9c2ff747badce9eb87189b548e0d03750e597a4e
Cyril_Lazar <klazar@mail.ru>
2024-03-04 22:15:54 +0700

    fix:поменял строку логов

--- logger.js
+++ logger.js
@@ -1,16 +1,17 @@
-const fs = require("fs");
 module.exports = class {
   constructor(path) {
     this.file = path;
   }
 
+  fs = require("fs");
+
   async write(content) {
     const timeStamp = (Date.now() / 1000).toFixed();
 
     await new Promise((res, rej) =>
-      fs.appendFile(
+      this.fs.appendFile(
         this.file,
-        JSON.stringify({ ...content, timeStamp }) + ",\n",
+        JSON.stringify({ ...content, timeStamp }) + "\n",
         (err) => {
           if (err) rej(err);
           res();
